70f666202ec1cf18a1e6b24846ad40fa0304eeec
- <->
2020-11-22 00:56:49 +0900

    20.11.22

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#define N 6
+
+int	init_othello(){
+	
+}
+
+int	isGameEnd(){
+	//if game end result 1
+}
+
+int	print_othello(){
+}
+
+int check_result(){
+}
+
+int gameboard[N][N];
+
+void main(){
+	
+	init_othello();
+	
+	while (isGameEnd()==0){
+		print_othello();
+		if()
+			continue;
+		
+		if()
+			if(){
+				printf("");
+				printf("");
+			}
+		else
+			printf("");
+	}
+	check_result();
+}
