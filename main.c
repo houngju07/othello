1d30c3dd30e801b2628450291ca066e071144d85
- <->
2020-11-24 20:46:31 +0900

    board
    board

--- main.c
+++ main.c
@@ -3,14 +3,50 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-#define N 6
+//#define N6
 
-int	init_othello(){
+//int	init_othello(){
 	
+int main(){
+	int i; 
+	int j;
+	int N;
+	int game_Board[N][N];
+	
+			printf("input even num N: ");
+			scanf("%i", &N);
+			
+		while((N%2)!= 0){
+				printf("N is not even number\n");
+				printf("input even num N: ");
+				scanf("%i", &N);
+			}
+	
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			game_Board[i][j] = 1;
+			printf("%i", game_Board[i][j]);
+		}	printf("\n");
+	}
+
+	return 0;
+	
+	//return game_Board[i][j];
 }
 
-int	isGameEnd(){
-	//if game end result 1
+/*int main(){
+	int N;
+	int Borad;
+	
+	printf("N: ");
+	scanf("%i", &N);
+	
+	printf("Board\n");
+	printf("%c", init_othello);
+}
+
+/*int	isGameEnd(){
+	if game end result 1
 }
 
 int	print_othello(){
@@ -39,4 +75,4 @@ void main(){
 			printf("");
 	}
 	check_result();
-}
+}*/
